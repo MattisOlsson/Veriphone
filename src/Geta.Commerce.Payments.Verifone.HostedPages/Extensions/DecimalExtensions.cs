0ca6c5f0fe4a9854b2ec400aa43629a36a533bae
Mattias Olsson <mattias@geta.no>
2018-06-27 17:06:40 +0200

    Fixed verifone amount string formatting.

--- src/Geta.Commerce.Payments.Verifone.HostedPages/Extensions/DecimalExtensions.cs
+++ src/Geta.Commerce.Payments.Verifone.HostedPages/Extensions/DecimalExtensions.cs
@@ -8,14 +8,7 @@ namespace Geta.Commerce.Payments.Verifone.HostedPages.Extensions
         public static string ToVerifoneAmountString(this decimal number)
         {
             var rounded = Math.Round(number, 2);
-            var veriphoneString = rounded.ToString(CultureInfo.InvariantCulture);
-
-            if (veriphoneString.IndexOf('.') == -1)
-            {
-                veriphoneString += "00";
-            }
-
-            return veriphoneString.Replace(".", "");
+            return rounded.ToString("F", CultureInfo.InvariantCulture).Replace(".", "");
         }
     }
 }
